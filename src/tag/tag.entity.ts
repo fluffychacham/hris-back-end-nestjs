6ef6c3fe37e2e3964f100b162eb40b0f222fa7e2
fluffychacham <chamtejido@gmail.com>
2019-12-21 17:41:23 -0700

    removed unused BeforeInsert from tag.entity.ts

--- src/tag/tag.entity.ts
+++ src/tag/tag.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, BeforeInsert} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
 @Entity('tag')
 export class TagEntity {
