07fa3c16d5d2399848f5fa162664daec4dbc3ecd
fluffychacham <chamtejido@gmail.com>
2019-12-26 01:43:18 -0700

    Added more paths for company

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -7,9 +7,10 @@ import { CompanyEntity } from "./company.entity";
 
 import { AuthMiddleware } from "../user/auth.middleware";
 import { UserModule } from "../user/user.module";
+import { UserEntity } from "../user/user.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([CompanyEntity]), UserModule],
+    imports: [TypeOrmModule.forFeature([CompanyEntity, UserEntity]), UserModule],
     providers: [CompanyService],
     controllers: [CompanyController],
     exports: [CompanyService]
@@ -18,6 +19,12 @@ export class CompanyModule implements NestModule {
     public configure(consumer: MiddlewareConsumer) {
         consumer
             .apply(AuthMiddleware)
-            .forRoutes({ path: "company", method: RequestMethod.GET }, { path: "company", method: RequestMethod.PUT });
+            .forRoutes(
+                { path: "companies", method: RequestMethod.GET }, 
+                { path: "company/:id", method: RequestMethod.GET }, 
+                { path: "company", method: RequestMethod.POST }, 
+                { path: "company/:id", method: RequestMethod.PUT },
+                { path: "company/:id", method: RequestMethod.DELETE }
+            );
     }
 }
